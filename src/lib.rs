336f8410d1c4f24d5d956774cdb077ac0bc38173
Berach <joey.mull1@gmail.com>
2015-07-25 18:42:12 +0100

    Fixed generation when seed is empty

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 extern crate redis;
+extern crate rand;
+use self::rand::{thread_rng, sample};
 use redis::Commands;
 
 #[no_mangle]
@@ -28,24 +30,54 @@ pub extern fn learn(con: &redis::Connection, input: &str) -> redis::RedisResult<
 
 #[no_mangle]
 pub extern fn generate(con: &redis::Connection, seed: &str) -> String {
-    let mut result = seed.to_string(); // Start our result with the seed passed to us
     let mut prev = "".to_string();
     let mut cur = seed.to_string();
+    let mut result = seed.to_string(); // Start our result with the seed passed to us
+    let mut rng = thread_rng();
 
     loop {
-        let key = make_key(&prev, &cur);
-        let members : Vec<String> = con.zrevrange(key, 0, -1).unwrap();
-        if members.len() > 0 && members[0] != "\n" {
+        let mut key = make_key(&prev, &cur);
+        let all_keys : Vec<String> = redis::cmd("KEYS").arg("*").query(con).unwrap();
+        println!("Current Key: {:?}", key);
+        if !all_keys.contains(&key) {
+            key = sample(&mut rng, all_keys.iter(), 1).pop().unwrap().clone();
+            let mut split : Vec<&str> = key.split(":").collect();
+            cur = split.pop().unwrap().to_string();
+            result.push_str(&cur);
+            println!("New Key: {:?}", key);
+        }
+        let members : Vec<(String, i32)> = con.zrevrange_withscores(key, 0, -1).unwrap();
+
+        let options = get_options(members);
+
+        let choice = sample(&mut rng, options.iter(), 1).pop().unwrap();
+
+        if choice != "\n" {
             result.push_str(" ");
-            result.push_str(&members[0]);
+            result.push_str(choice);
             prev = cur;
-            cur = members[0].clone();
+            cur = choice.clone();
         } else {
             break;
         }
     }
 
     result
+}
+
+fn get_options(members: Vec<(String, i32)>) -> Vec<String> {
+    let mut options : Vec<String> = Vec::new();
+    let mut prev_score = 0;
+
+    for (member, score) in members {
+        if score < prev_score {
+            break;
+        } else {
+            options.push(member);
+            prev_score = score;
+        }
+    }
+    options
 }
 
 /// Takes two words and joins them with colons
